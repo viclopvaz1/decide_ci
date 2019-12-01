2efca374ddcbde92dcb30f679ce93346619c3a16
José A <malawito@gmail.com>
2019-12-01 19:48:29 +0100

    Update settings.py
    reduce keysize

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -174,7 +174,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
-KEYBITS = 256
+KEYBITS = 192
 
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
