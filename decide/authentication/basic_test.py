ae59f0d37f50a9d7c321ede1cb5da6cab27a1d53
jagalindo <jagalindo@us.es>
2019-12-01 13:15:36 +0100

    Adding a simple failing tests (1+1=3)

--- decide/authentication/basic_test.py
+++ decide/authentication/basic_test.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
 class SimpleTest(TestCase):
     def test_basic_addition(self):
-    """
-    Tests that 1 + 1 always equals 2.
-    """
-    self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 3)
