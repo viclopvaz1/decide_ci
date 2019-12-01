0f1205ef7a66270a507436442f53967574789af3
jagalindo <jagalindo@us.es>
2019-12-01 16:35:50 +0100

    fixing mixnet

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'heroku-meet-travis.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
